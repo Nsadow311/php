3d4eb333ed590832e876a87d426dc46a32a8af4e
Nsadow311 <63483406+Nsadow311@users.noreply.github.com>
2021-02-23 03:31:02 +0000

    Add files via upload

---
+++ shared.lua
@@ -0,0 +1,7 @@
+ENT.Type = "anim"
+ENT.Base = "base_entity"
+ENT.PrintName = "Recruit Stopper"
+ENT.Author = "Anthony Fuller"
+ENT.Category = "Recruit Stopper"
+ENT.Spawnable = true
+ENT.Contact = "A admin"
